7ebe146a1a5fcdfcd96b2638059209d2242b1346
Melnikov Ruslan <Melnikovruslan17@yandex.ru>
2022-09-28 21:45:12 +0300

    Initial commit

---
+++ src/Worker.java
@@ -0,0 +1,36 @@
+
+public class Worker implements OnTaskDoneListener, OnTaskErrorListener {
+    @Override
+    public void onDone(String result) {
+
+    }
+
+    @Override
+    public void onError(String error) {
+
+    }
+
+    private OnTaskDoneListener callback;
+    private OnTaskErrorListener errorCallback;
+
+
+    public Worker(OnTaskDoneListener callback, OnTaskErrorListener errorCallback) {
+        this.callback = callback;
+        this.errorCallback = errorCallback;
+
+    }
+
+    public void start() {
+        for (int i = 0; i < 100; i++) {
+            if (i == 33) {
+                errorCallback.onError("Task " + i + " is not done");
+                continue;
+            }
+            callback.onDone("Task " + i + " is done");
+        }
+    }
+
+
+}
+
+
